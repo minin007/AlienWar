c2fddd2f7c6e266cab2ee037dadc1aad9c43badc
minin007 <764802547@qq.com>
2020-02-23 23:09:15 +0800

    update basic files

---
+++ game_function.py
@@ -0,0 +1,17 @@
+import sys
+import pygame
+
+def check_events():
+    """响应按键和鼠标事件"""
+
+
+
+def update_screen():
+    """更新屏幕上的图像，并切换到新屏幕"""
+
+
+def creat_fleet():
+    """创建外星人群"""
+
+def check_edges():
+    """边缘响应、移动响应、移动"""
